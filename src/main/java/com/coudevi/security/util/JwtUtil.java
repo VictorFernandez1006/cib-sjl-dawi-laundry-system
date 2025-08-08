e285d43a69003a5b61e5bcc0b0904e692faef51b
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-08 13:43:58 -0500

    feature: Add jwt util for build and validate tokens and claims

---
+++ src/main/java/com/coudevi/security/util/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.coudevi.security.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration}")
+    private long expirationMs;
+    private final String CLAIM_ROLE = "roles";
+
+    private Key getSigninKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = Map.of(
+                CLAIM_ROLE, userDetails.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList()
+        );
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public String extractUsername(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token){
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public boolean validateToken(String token, UserDetails user){
+        String username = extractUsername(token);
+        Date expiration = extractExpiration(token);
+        return username.equals(user.getUsername()) && expiration.after(new Date());
+    }
+}
