060864ae4fc1e75a3fcee7120b3257d3ee5ff733
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-08 13:44:24 -0500

    feature: Add repositories for entities

---
+++ src/main/java/com/coudevi/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.coudevi.domain.repository;
+
+import com.coudevi.domain.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
